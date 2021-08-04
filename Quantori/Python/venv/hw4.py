c7f516ec85117d8bb30e34289e0cf0c72006dc39
Simyrun <61234508+Simyrun@users.noreply.github.com>
2021-08-04 15:03:19 +0900

    Delete Quantori/Python/venv directory

--- Quantori/Python/venv/hw4.py
+++
@@ -1,7 +0,0 @@
-import re
-text = input("write your line : ")
-regular = r"\d+"
-# если быть честным, то тут происходит магия из интернета (когда нибудь я её пойму).
-filtertext = list(map(int, re.findall('-?[0-9]+', text)))
-result= sum(filtertext)
-print(result)
